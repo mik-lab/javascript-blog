1b075d1560d622ff3c51e0ed13baa440dd0040d2
Mikołaj <121145556+mik-lab@users.noreply.github.com>
2023-01-23 22:18:54 +0100

    Remove articles from html and add script.js

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function titleClickHandler(event){
 
   /* [DONE] get 'href' attribute from the clicked link */
 
-  const articleSelector = this.getAttribute('href');
+  const articleSelector = clickedElement.getAttribute('href');
   console.log(articleSelector);
 
   /*[DONE] find the correct article using the selector (value of 'href' attribute) */
@@ -47,3 +47,36 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const links = document.querySelector(optTitleListSelector);
+  const titleList = links;
+  titleList.innerHTML = '';
+
+  /* for each article */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+  for(let article of articles){
+    
+  }
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
